f99af71a29ebf7dfc9f7ecd021823d09fab34f55
Mark Thompson <mthommo79@gmail.com>
2025-09-03 05:38:34 +1000

    Initial commit

---
+++ scenes/characters/player/player.gd
@@ -0,0 +1,14 @@
+class_name Player
+extends CharacterBody2D
+
+@export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+@onready var hit_component = $HitComponent
+
+var player_direction: Vector2
+
+func _ready():
+	ToolManager.tool_selected.connect(on_tool_selected)
+
+func on_tool_selected(tool: DataTypes.Tools) -> void:
+	current_tool = tool
+	hit_component.current_tool = tool

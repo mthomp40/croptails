55ed4c08d2090a0928c273d355a59aee9c210215
Mark Thompson <mthommo79@gmail.com>
2025-09-05 07:03:50 +1000

    Add chest pickup and dialogue

--- scenes/ui/emotes_panel.gd
+++ scenes/ui/emotes_panel.gd
@@ -8,7 +8,6 @@ var idle_emotes: Array = ["emote_1_idle", "emote_2_smile", "emote_3_ear_wave", "
 
 func _ready() -> void:
 	animated_sprite_2d.play("emote_1_idle")
-	print("emotes_panel ready")
 	emote_idle_timer.timeout.connect(_on_emote_idle_timer_timeout)
 	InventoryManager.inventory_changed.connect(on_inventory_changed)
 	GameDialogueManager.feed_the_animals.connect(on_feed_the_animals)
@@ -16,12 +15,10 @@ func _ready() -> void:
 
 func play_emote(animation: String) -> void:
 	animated_sprite_2d.play(animation)
-	print("emotes_panel play_emote: ", animation)
 
 func _on_emote_idle_timer_timeout() -> void:
 	var index = randi_range(0,3)
 	var emote = idle_emotes[index]
-	print("emotes_panel _on_emote_idle_timer_timeout: ", emote)
 	animated_sprite_2d.play(emote)
 
 func on_inventory_changed() -> void:
--- scripts/globals/game_dialogue_manager.gd
+++ scripts/globals/game_dialogue_manager.gd
@@ -3,7 +3,6 @@ extends Node
 signal give_crop_seeds
 signal feed_the_animals
 
-
 func action_give_crop_seeds() -> void:
 	give_crop_seeds.emit()
 
